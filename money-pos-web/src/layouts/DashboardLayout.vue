e88f63ee682a3a403df3f126ba03f9ab74dc617b
money <374648769@qq.com>
2026-02-17 14:02:19 +0800

    fix(DashboardLayout): 修复侧边栏状态变化时布局错位问题
    根据侧边栏的打开和悬停状态动态调整内容区域的左边距，避免布局错位

--- money-pos-web/src/layouts/DashboardLayout.vue
+++ money-pos-web/src/layouts/DashboardLayout.vue
@@ -11,7 +11,13 @@ import Footer from './footer/Footer.vue';
 
         <div
             style="transition-property: margin-left; transition-duration: 150ms"
-            class="flex-1 flex flex-col ml-0 md:ml-16 lg:ml-60"
+            :class="[
+                'flex-1 flex flex-col',
+                {
+                    'ml-0 md:ml-16': !sidebarState.isOpen && !sidebarState.isHovered,
+                    'ml-0 md:ml-60': sidebarState.isOpen || sidebarState.isHovered,
+                }
+            ]"
         >
             <Navbar />
             <div class="flex-1 p-2">
